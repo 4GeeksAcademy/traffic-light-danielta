f5a06d49d1d74946e0498d7b9891ba5232b17daf
danielta18 <153893558+danielta18@users.noreply.github.com>
2024-10-25 16:55:28 +0000

    first commit

--- src/js/component/home.jsx
+++ src/js/component/home.jsx
@@ -1,26 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 
 //include images into your bundle
 import rigoImage from "../../img/rigo-baby.jpg";
 
 //create your first component
-const Home = () => {
-	return (
-		<div className="text-center">
-			<h1 className="text-center mt-5">Hello Rigo!</h1>
-			<p>
-				<img src={rigoImage} />
-			</p>
-			<a href="#" className="btn btn-success">
-				If you see this green button... bootstrap is working...
-			</a>
-			<p>
-				Made by{" "}
-				<a href="http://www.4geeksacademy.com">4Geeks Academy</a>, with
-				love!
-			</p>
-		</div>
+const TrafficLight = () => {
+	const [color, setColor] = useState("red")  
+	// if (color == "red")
+		return (
+			<div id="light">
+				<button type="button" id="redLight" style={{backgroundColor: "red", boxShadow: color == "red" && "0 0 10px 15px white"}} onClick= {() => setColor("red")}></button>
+				<button type="button" id="yellowLight" style={{backgroundColor: "yellow", boxShadow: color == "yellow" && "0 0 10px 15px white"}} onClick= {() => setColor("yellow")}></button>
+				<button type="button" id="greenLight" style={{backgroundColor: "green", boxShadow: color == "green" && "0 0 10px 15px white"}} onClick= {() => setColor("green")}></button>
+			</div>
 	);
+	// if (color == "yellow")
+	// 	return (
+	// 		<div id="light">
+	// 			<button type="button" id="redLight" style={{backgroundColor: "red"}} onClick= {() => setColor("red")}></button>
+	// 			<button type="button" id="yellowLight" style={{backgroundColor: "yellow", boxShadow: "0 0 10px 15px white"}} onClick= {() => setColor("yellow")}></button>
+	// 			<button type="button" id="greenLight" style={{backgroundColor: "green"}} onClick= {() => setColor("green")}></button>
+	// 		</div>
+	// );
+	// if (color == "green")
+	// 	return (
+	// 		<div id="light">
+	// 			<button type="button" id="redLight" style={{backgroundColor: "red"}} onClick= {() => setColor("red")}></button>
+	// 			<button type="button" id="yellowLight" style={{backgroundColor: "yellow"}} onClick= {() => setColor("yellow")}></button>
+	// 			<button type="button" id="greenLight" style={{backgroundColor: "green", boxShadow: "0 0 10px 15px white"}} onClick= {() => setColor("green")}></button>
+	// 		</div>
+	// );
 };
 
-export default Home;
+export default TrafficLight;
